02649b51c9b5087c225fc4be6cdc582059d70efa
Lakisha Trusty <daprincessanime@gmail.com>
2021-10-14 17:42:08 -0400

    add movie poster to the movie selector and add the li to ul

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ fetch("https://ghibliapi.herokuapp.com/films")
     return res.json()
 })
 .then((movies)=>{
-    // console.log(movies);
+    console.log(movies);
 
 
 let dropDown =document.querySelector("#dropdown");
@@ -14,15 +14,26 @@ for(let movie of movies){
     option.textContent = movie.title;
     dropDown.append(option);
 }
+ let selectedMovie;
+const engTitle = document.querySelector("#movie-title");
+const yearRelease = document.querySelector("#year-release");
+const movieDescr =document.querySelector("#descriptions");
 
 
 dropDown.addEventListener("change", ()=>{
     for(let movie of movies){
         if(dropDown.value === movie.title){
-
+            selectedMovie = movie;
+            engTitle.textContent = movie.title;
+            yearRelease.textContent = movie.release_date;
+            movieDescr.textContent = movie.description;
+            
         }
     }
 })
+let listReview =document.querySelector("ul")
+let myReview = document.createElement("li")
+
 })
 
 
